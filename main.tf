e25e19c161b4eb25b8cb336d223966eae05ff98c
varshneysan001 <59354572+varshneysan001@users.noreply.github.com>
2019-12-31 11:47:02 +0530

    tf version update

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ provider "aws" {
 	region = "${var.vpc_region}"
 	access_key = "${var.aws_access_key}"
   	secret_key = "${var.aws_secret_key}"
+	version	   = "~> 2.43"	
 }
 
 resource "aws_vpc" "vpc" {
